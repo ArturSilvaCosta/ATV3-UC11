f113b44275f3539b94286fecb3c1cc6a77206c44
Artur Costa <a.c.tiotucao@gmail.com>
2026-03-13 10:13:47 -0300

    Projeto inicial ATV3

---
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -0,0 +1,55 @@
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+
+public class ProdutosDAO {
+    Connection conn;
+    PreparedStatement prep;
+    ResultSet resultset;
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    
+    public void cadastrarProduto (ProdutosDTO produto){
+        conn = new conectaDAO().connectDB();
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)";
+        
+        try{
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!");
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar: "+e.getLocalizedMessage());
+        }
+ 
+    }
+    
+    public ArrayList<ProdutosDTO> listarProdutos(){
+        String sql = "SELECT * FROM produtos";
+        conn = new conectaDAO().connectDB();
+        
+        try{
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            while (resultset.next()){
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                
+                listagem.add(produto);
+            }
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Erro ao listar os produtos: "+e.getMessage());
+        }
+        
+        return listagem;
+    }
+}
+
